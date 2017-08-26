61fbf8a5d380477f5d7a32ccf3ac318d1cc318e8
Kunal Dabir <kunal.dabir@gmail.com>
2017-08-26 12:25:06 +0530

    access cli args and env vars

--- script.groovy
+++ script.groovy
@@ -1,2 +1,9 @@
 def message = ['*' * 80, 'hello world! any groovy script can execute here', '*' * 80].join('\n')
 println message
+
+println "Access env variables : ${System.env.CI}" 
+println "Access command line args : ${args[0]}" 
+
+
+
+
